95fd0129a38394d3ef1213216c1fdbe10abb9723
Andrea Perego <andrea-perego@users.noreply.github.com>
2019-02-18 00:02:50 +0100

    Create config.js

---
+++ ir-vocab-dcat-2/config.js
@@ -0,0 +1,13 @@
+var respecConfig = {
+  specStatus: "base",
+  editors: [
+    {
+      name: "John Doe",
+      company: "some company",
+      companyURL: "http://www.example.com/"
+    }
+  ],
+  processVersion: 2019,
+  edDraftURI: "https://w3c.github.io/dxwg/dcat-implementation-report/",
+  shortName: "ir-vocab-dcat-2"
+};
